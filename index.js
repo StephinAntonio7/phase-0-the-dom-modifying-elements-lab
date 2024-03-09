2c96a0702c8435b0423dd3ed06b720e1d65fe071
Stephin Roberts <stephin.roberts@gmail.com>
2024-03-09 14:54:33 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+   mainElement.remove();
+
+const newHeader = document.createElement('h1');
+   newHeader.textContent = 'This is the new header';
+   newHeader.textContent = 'Stephin Roberts is the champion'; 
+   newHeader.id = 'victory';
+   document.body.appendChild(newHeader);
